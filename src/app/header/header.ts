40314a7d07184ebd816237326cb69718a3416da0
Jesus Raul Galaviz Lugo <l20170879@culiacan.tecnm.mx>
2025-10-22 12:03:59 -0700

    Indice

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
 export class Header {
-  
+  isDropdownOpen = false;
 }
